98d1254c14f403fd4329492e9fcccb9bb2d45cf1
patil2neha2 <patil2neha2@gmail.com>
2023-12-05 16:55:46 +0530

    Add project files.

---
+++ CodePulse.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CodePulse.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace CodePulse.API.Data
+{
+   
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<BlogImage> BlogImages { get; set; }
+    }
+}
---
+++ CodePulse.API/Migrations/AuthDb/20231201061217_auth.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodePulse.API.Migrations.AuthDb
+{
+    /// <inheritdoc />
+    public partial class auth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "edc267ec-d43c-4e3b-8108-a1a1f819906d",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "b2532ed3-ddaa-4c55-8b47-90e1c27a08a6", "AQAAAAIAAYagAAAAEFHSdkplTrQzubsy4krwCh2Vfa6+d0t2ZDluLX546a5BCnbp18zVb2kfTY8RO8hfUQ==", "c35ae4c7-9d01-4724-88f7-32abb14bb594" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "edc267ec-d43c-4e3b-8108-a1a1f819906d",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "36133d24-05e9-47a7-92e7-26a3a04657c0", "AQAAAAIAAYagAAAAEFFugz4+/jgC5expBuZd0vaXRKHu8AQ7CVNbM+H1sHeylsaLv2GJzpByC5lk785NFQ==", "17a80c1d-54b0-4a38-836d-07d816449916" });
+        }
+    }
+}
